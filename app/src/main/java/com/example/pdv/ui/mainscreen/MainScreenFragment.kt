aa0fe3b87340c17c372095e3da8205d6da00a5f5
StellarBits <stellarbitsapps@gmail.com>
2023-11-07 23:04:12 -0300

    Some layout improvements in main screen.

--- app/src/main/java/com/example/pdv/ui/mainscreen/MainScreenFragment.kt
+++ app/src/main/java/com/example/pdv/ui/mainscreen/MainScreenFragment.kt
@@ -1,17 +1,23 @@
 package com.example.pdv.ui.mainscreen
 
+import android.annotation.SuppressLint
 import android.app.AlertDialog
+import android.graphics.drawable.Drawable
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.view.menu.MenuBuilder
+import androidx.core.content.ContextCompat
+import androidx.core.graphics.drawable.DrawableCompat
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.pdv.R
 import com.example.pdv.databinding.FragmentMainScreenBinding
+
 
 class MainScreenFragment : Fragment() {
 
@@ -31,12 +37,48 @@ class MainScreenFragment : Fragment() {
     ): View {
         setHasOptionsMenu(true)
 
+        binding.salesButton.setOnClickListener {
+            findNavController().navigate(R.id.salesFragment)
+        }
+
+        binding.syncButton.setOnClickListener {
+            findNavController().navigate(R.id.synchronizationFragment)
+        }
+
+        binding.reportsButton.setOnClickListener {
+            findNavController().navigate(R.id.reportFragment)
+        }
+
+        binding.registrationsButton.setOnClickListener {
+            findNavController().navigate(R.id.registrationsFragment)
+        }
+
         return binding.root
     }
 
+    @SuppressLint("RestrictedApi")
     @Deprecated("Deprecated in Java")
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         inflater.inflate(R.menu.settings_menu, menu)
+
+        if (menu is MenuBuilder) {
+            menu.setOptionalIconsVisible(true)
+        }
+
+        // Set colors to menu items
+        val drawables = listOf(
+            menu.findItem(R.id.print_layout),
+            menu.findItem(R.id.tef_settings),
+            menu.findItem(R.id.tax_settings),
+            menu.findItem(R.id.network_settings)
+        )
+
+        drawables.forEach {
+            val drawable = DrawableCompat.wrap(it.icon!!)
+            DrawableCompat.setTint(drawable, ContextCompat.getColor(requireContext(), R.color.light_blue))
+            it.icon = drawable
+        }
+
         super.onCreateOptionsMenu(menu, inflater)
     }
 
